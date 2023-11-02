8d89b3fc6a554e32fc61dbd3cea09ef312405bf2
diya92003 <147637741+diya92003@users.noreply.github.com>
2023-11-02 19:25:31 +0530

    Create courseController.js

---
+++ Controllers/courseController.js
@@ -0,0 +1,13 @@
+const { response } = require("express");
+const CourseModel = require("../Models/courseModel");
+
+let createCourse = async (req, res) => {
+    try {
+        let data = req.body
+        let registerCourse = await CourseModel.create(data)
+        return res.status(201).send({status:true, msg:"Course Data registered successfully", data:registerCourse});
+    } catch (error) {
+        return res.status(500).send({status: false, msg: "Internal Server Error" });
+    }
+};
+module.exports = {createCourse}
